3378b79e76bb3289b968b65eeb15830407aa556a
Katon Fitrianto <katonadams.f@gmail.com>
2021-11-11 20:54:25 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import './App.css'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Routes from './Routes'
-import Sidebar from './component/sidebar/Sidebar'
-import TopNav from './component/TopNav'
-import Login from './pages/Login'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Navbar } from './component/Nabvar'
+import { Footer } from './component/Footer'
+import { Login } from './component/Login'
+import { Home } from './component/Home'
+import { DetailProduct } from './component/DetailProduct'
 
 function App() {
+  
+  useEffect(() => {
+    document.title = `Toko Jaya`
+  })
 
   return (
     <Router>
       <Switch>
-        <Route path='/login' exact component={Login} />
-        {/* <Route path='/' exact component={Sidebar} /> */}
-        <Route
-          render={(props) => (
-            <div className="App">
-              <Sidebar {...props} />
-              <div className='layout-content'>
-                <TopNav />
-
-                <div className='layout-content-main'>
-                  <Routes />
-                </div>
-              </div>
-            </div>
-          )}
-        ></Route>
+        <Route exact path="/login" component={Login} />
+        <div className="App">
+          <Navbar />
+            <Route path="/product/:productId" exact component={DetailProduct} />
+            <Route path="/" exact component={Home} />
+            {/* <Route>404 Not Found!</Route> */}
+          <Footer />
+        </div>
       </Switch>
     </Router>
   );
